476715bf2190f4450d31cf4c03b2e9f7874d7f4a
Denis Lupu <foolcat@protonmail.ch>
2024-12-09 17:21:58 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,6 @@ if not vim.g.vscode then
   vim.o.termguicolors = true
   vim.o.background = "dark"
   vim.g.ai_cmp = false
-  vim.cmd("colorscheme darcula")
+  vim.cmd("colorscheme darcula-dark")
   vim.cmd("set guifont=Iosevka:17")
 end
