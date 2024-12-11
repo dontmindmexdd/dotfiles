b4ea22db7cbdc0a71c60568803284304c6a8d7c4
Denis Lupu <foolcat@protonmail.ch>
2024-12-11 18:48:40 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
-	{ family = "Input Mono Compressed", weight = "Medium" },
+	{ family = "Input Mono Compressed", weight = "Regular" },
 })
 config.font_size = 19
 
